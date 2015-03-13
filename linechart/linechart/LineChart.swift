138d0fc1741c8205ffa38f7728ffb94466fbf21f
Mirco Zeiss <mirco.zeiss@gmail.com>
2015-03-13 18:06:19 +0100

    move everything into one file

--- linechart/linechart/LineChart.swift
+++ linechart/linechart/LineChart.swift
@@ -579,3 +579,124 @@ public class LineChart: UIView {
         self.setNeedsDisplay()
     }
 }
+
+
+
+/**
+ * DotCALayer
+ */
+class DotCALayer: CALayer {
+    
+    var innerRadius: CGFloat = 8
+    var dotInnerColor = UIColor.blackColor()
+    
+    override init() {
+        super.init()
+    }
+    
+    override init(layer: AnyObject!) {
+        super.init(layer: layer)
+    }
+    
+    required init(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    override func layoutSublayers() {
+        super.layoutSublayers()
+        var inset = self.bounds.size.width - innerRadius
+        var innerDotLayer = CALayer()
+        innerDotLayer.frame = CGRectInset(self.bounds, inset/2, inset/2)
+        innerDotLayer.backgroundColor = dotInnerColor.CGColor
+        innerDotLayer.cornerRadius = innerRadius / 2
+        self.addSublayer(innerDotLayer)
+    }
+    
+}
+
+
+
+/**
+ * LinearScale
+ */
+public class LinearScale {
+    
+    var domain: [CGFloat]
+    var range: [CGFloat]
+    
+    public init(domain: [CGFloat] = [0, 1], range: [CGFloat] = [0, 1]) {
+        self.domain = domain
+        self.range = range
+    }
+    
+    public func scale() -> (x: CGFloat) -> CGFloat {
+        return bilinear(domain, range: range, uninterpolate: uninterpolate, interpolate: interpolate)
+    }
+    
+    public func invert() -> (x: CGFloat) -> CGFloat {
+        return bilinear(range, range: domain, uninterpolate: uninterpolate, interpolate: interpolate)
+    }
+    
+    public func ticks(m: Int) -> (CGFloat, CGFloat, CGFloat) {
+        return scale_linearTicks(domain, m: m)
+    }
+    
+    private func scale_linearTicks(domain: [CGFloat], m: Int) -> (CGFloat, CGFloat, CGFloat) {
+        return scale_linearTickRange(domain, m: m)
+    }
+    
+    private func scale_linearTickRange(domain: [CGFloat], m: Int) -> (CGFloat, CGFloat, CGFloat) {
+        var extent = scaleExtent(domain)
+        var span = extent[1] - extent[0]
+        var step = CGFloat(pow(10, floor(log(Double(span) / Double(m)) / M_LN10)))
+        var err = CGFloat(m) / span * step
+        
+        // Filter ticks to get closer to the desired count.
+        if (err <= 0.15) {
+            step *= 10
+        } else if (err <= 0.35) {
+            step *= 5
+        } else if (err <= 0.75) {
+            step *= 2
+        }
+        
+        // Round start and stop values to step interval.
+        var start = ceil(extent[0] / step) * step
+        var stop = floor(extent[1] / step) * step + step * 0.5 // inclusive
+        
+        return (start, stop, step)
+    }
+    
+    private func scaleExtent(domain: [CGFloat]) -> [CGFloat] {
+        var start = domain[0]
+        var stop = domain[count(domain) - 1]
+        return start < stop ? [start, stop] : [stop, start]
+    }
+    
+    private func interpolate(a: CGFloat, b: CGFloat) -> (c: CGFloat) -> CGFloat {
+        var diff = b - a
+        func f(c: CGFloat) -> CGFloat {
+            return (a + diff) * c
+        }
+        return f
+    }
+    
+    private func uninterpolate(a: CGFloat, b: CGFloat) -> (c: CGFloat) -> CGFloat {
+        var diff = b - a
+        var re = diff != 0 ? 1 / diff : 0
+        func f(c: CGFloat) -> CGFloat {
+            return (c - a) * re
+        }
+        return f
+    }
+    
+    private func bilinear(domain: [CGFloat], range: [CGFloat], uninterpolate: (a: CGFloat, b: CGFloat) -> (c: CGFloat) -> CGFloat, interpolate: (a: CGFloat, b: CGFloat) -> (c: CGFloat) -> CGFloat) -> (c: CGFloat) -> CGFloat {
+        var u: (c: CGFloat) -> CGFloat = uninterpolate(a: domain[0], b: domain[1])
+        var i: (c: CGFloat) -> CGFloat = interpolate(a: range[0], b: range[1])
+        func f(d: CGFloat) -> CGFloat {
+            return i(c: u(c: d))
+        }
+        return f
+    }
+    
+}

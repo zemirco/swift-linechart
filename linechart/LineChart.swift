47a8a9cb1dc1ee157b8bba04d910f110b05c6c3f
Mirco Zeiss <mirco.zeiss@gmail.com>
2015-09-06 15:55:05 +0200

    move everything under the LineChart namespace

--- linechart/LineChart.swift
+++ linechart/LineChart.swift
@@ -4,95 +4,89 @@
 import UIKit
 import QuartzCore
 
-
-
-/**
- * Helpers class
- */
-private class Helpers {
-    
-    /**
-     * Convert hex color to UIColor
-     */
-    private class func UIColorFromHex(hex: Int) -> UIColor {
-        let red = CGFloat((hex & 0xFF0000) >> 16) / 255.0
-        let green = CGFloat((hex & 0xFF00) >> 8) / 255.0
-        let blue = CGFloat((hex & 0xFF)) / 255.0
-        return UIColor(red: red, green: green, blue: blue, alpha: 1)
-    }
-    
-    /**
-     * Lighten color.
-     */
-    private class func lightenUIColor(color: UIColor) -> UIColor {
-        var h: CGFloat = 0
-        var s: CGFloat = 0
-        var b: CGFloat = 0
-        var a: CGFloat = 0
-        color.getHue(&h, saturation: &s, brightness: &b, alpha: &a)
-        return UIColor(hue: h, saturation: s, brightness: b * 1.5, alpha: a)
-    }
-}
-
-
-
 // delegate method
 public protocol LineChartDelegate {
     func didSelectDataPoint(x: CGFloat, yValues: [CGFloat])
 }
 
-public struct Labels {
-    public var visible: Bool = true
-    public var values: [String] = []
-}
-
-public struct Grid {
-    public var visible: Bool = true
-    public var count: CGFloat = 10
-    // #eeeeee
-    public var color: UIColor = UIColor(red: 238/255.0, green: 238/255.0, blue: 238/255.0, alpha: 1)
-}
-
-public struct Axis {
-    public var visible: Bool = true
-    // #607d8b
-    public var color: UIColor = UIColor(red: 96/255.0, green: 125/255.0, blue: 139/255.0, alpha: 1)
-    public var inset: CGFloat = 15
-}
-
-public struct Coordinate {
-    // public
-    public var labels: Labels = Labels()
-    public var grid: Grid = Grid()
-    public var axis: Axis = Axis()
-    
-    // private
-    private var linear: LinearScale!
-    private var scale: ((CGFloat) -> CGFloat)!
-    private var invert: ((CGFloat) -> CGFloat)!
-    private var ticks: (CGFloat, CGFloat, CGFloat)!
-}
-
-public struct Animation {
-    public var enabled: Bool = true
-    public var duration: CFTimeInterval = 1
-}
-
-public struct Dots {
-    public var visible: Bool = true
-    public var color: UIColor = UIColor.whiteColor()
-    public var innerRadius: CGFloat = 8
-    public var outerRadius: CGFloat = 12
-    public var innerRadiusHighlighted: CGFloat = 8
-    public var outerRadiusHighlighted: CGFloat = 12
-}
-
-
-
 /**
  * LineChart
  */
 public class LineChart: UIView {
+    
+    /**
+    * Helpers class
+    */
+    private class Helpers {
+        
+        /**
+        * Convert hex color to UIColor
+        */
+        private class func UIColorFromHex(hex: Int) -> UIColor {
+            let red = CGFloat((hex & 0xFF0000) >> 16) / 255.0
+            let green = CGFloat((hex & 0xFF00) >> 8) / 255.0
+            let blue = CGFloat((hex & 0xFF)) / 255.0
+            return UIColor(red: red, green: green, blue: blue, alpha: 1)
+        }
+        
+        /**
+        * Lighten color.
+        */
+        private class func lightenUIColor(color: UIColor) -> UIColor {
+            var h: CGFloat = 0
+            var s: CGFloat = 0
+            var b: CGFloat = 0
+            var a: CGFloat = 0
+            color.getHue(&h, saturation: &s, brightness: &b, alpha: &a)
+            return UIColor(hue: h, saturation: s, brightness: b * 1.5, alpha: a)
+        }
+    }
+    
+    public struct Labels {
+        public var visible: Bool = true
+        public var values: [String] = []
+    }
+    
+    public struct Grid {
+        public var visible: Bool = true
+        public var count: CGFloat = 10
+        // #eeeeee
+        public var color: UIColor = UIColor(red: 238/255.0, green: 238/255.0, blue: 238/255.0, alpha: 1)
+    }
+    
+    public struct Axis {
+        public var visible: Bool = true
+        // #607d8b
+        public var color: UIColor = UIColor(red: 96/255.0, green: 125/255.0, blue: 139/255.0, alpha: 1)
+        public var inset: CGFloat = 15
+    }
+    
+    public struct Coordinate {
+        // public
+        public var labels: Labels = Labels()
+        public var grid: Grid = Grid()
+        public var axis: Axis = Axis()
+        
+        // private
+        private var linear: LinearScale!
+        private var scale: ((CGFloat) -> CGFloat)!
+        private var invert: ((CGFloat) -> CGFloat)!
+        private var ticks: (CGFloat, CGFloat, CGFloat)!
+    }
+    
+    public struct Animation {
+        public var enabled: Bool = true
+        public var duration: CFTimeInterval = 1
+    }
+    
+    public struct Dots {
+        public var visible: Bool = true
+        public var color: UIColor = UIColor.whiteColor()
+        public var innerRadius: CGFloat = 8
+        public var outerRadius: CGFloat = 12
+        public var innerRadiusHighlighted: CGFloat = 8
+        public var outerRadiusHighlighted: CGFloat = 12
+    }
     
     // default configuration
     public var area: Bool = true
